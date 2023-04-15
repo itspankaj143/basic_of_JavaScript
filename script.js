dbec60445f8efba5683b9ddc9b05b132928d6e69
Pankaj Kumar <124787647+itspankaj143@users.noreply.github.com>
2023-04-15 23:56:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+// 7;
+// 2.5;
+// Datatype: Number;
+
+// "anurag" 'anurag'
+
+// Datatype: String
+
+// true
+// false
+// Name: George Boole
+// Datatype: Boolean
+
+// null
+// undefined
+// Datatype: Empty Values
+
+// [1,2,"Anurag",true, false, null]
+// Datatype: Array
+
+// Object
+// {key:value}
+// {fname:"anurag",lname: "tiwari"}
+
+//Variables
+
+// var username = "Anurag";
+
+// let number = 25;
+
+// const companyname = "iNeuron";
+
+// console.log(username);
+
+// console.log(typeof lastname);
+
+// console.log("Name is " + username + "Number is" + number);
+
+// interpolation / Template Literal / Template Strings
+
+// console.log(`
+//     My Name is ${username}
+//     My Number is ${number}
+
+// `);
+
+// console.log(username.length);
+// console.log(username[3]);
+
+// operator
+// +, - , *, /, % , **  2 ** 3
+
+// ==, === !=
+
+// let One = 1;
+// let two = "1";
+// console.log(One == two);
+
+// logical OP
+// && ||
+
+// Conditons
+// Mini app => Rating App
+// let rating = 4;
+// if (rating == 5) {
+//   console.log("5 Stars");
+// } else if (rating == 4) {
+//   console.log("4 Stars");
+// } else {
+//   console.log("We will improve");
+// }
+
+// ternary Conditions (oper)
+
+// // condition ? exep(true) : exp(false);
+
+// login = true;
+// login ? console.log("Logout") : console.log("Login First");
+
+// truthy Value , Falsy values
+
+// Math Object
+
+// const anurag = Math.PI;
+// console.log(Math.PI);
+console.log(Math.floor(Math.random() * 50)); // 0 - 0.9999999999
